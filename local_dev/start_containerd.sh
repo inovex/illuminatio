4483070cb10126a9afaa3920ad4e9f563a6a7ade
Johannes Scheuermann <johscheuer@users.noreply.github.com>
2019-08-21 13:14:31 +0200

    Use minikube in ci (#45)
    * Test minikube in CI
    
    Test minikube in CI with Docker
    
    * Fix containerd tests
    
    * Fail if curl get's an http error
    
    * Adjust script for CI
    
    * Cleanup env variable duplicates
    
    * Fix env var
    
    * Fix env var

--- local_dev/start_containerd.sh
+++ local_dev/start_containerd.sh
@@ -2,7 +2,10 @@
 # abort if any of the following commands fails or variables are undefined
 set -eu
 
-#setup minikube
+KUBERNETES_VERSION="${KUBERNETES_VERSION:-v1.15.0}"
+CALICO_VERSION="${CALICO_VERSION:-v3.8}"
+
+# Setup minikube
 minikube delete
 minikube start \
     --memory 4096 \
@@ -19,15 +22,32 @@ minikube start \
     --extra-config=controller-manager.cluster-cidr=192.168.0.0/16 \
     --bootstrapper=kubeadm \
     --host-only-cidr=172.17.17.1/24 \
-    --insecure-registry=localhost:5000
+    --insecure-registry=localhost:5000 \
+    --kubernetes-version="${KUBERNETES_VERSION}"
 
-#setup the minikube docker registry and calico
+# Setup the minikube docker registry and calico
 minikube addons enable registry
-kubectl apply -f https://docs.projectcalico.org/v3.8/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
+
+if [[ -n ${CI} ]];
+then
+    sudo chown -R travis: /home/travis/.minikube/
+fi
+
+kubectl apply -f "https://docs.projectcalico.org/${CALICO_VERSION}/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml"
 kubectl apply -f local_dev/docker-registry.yml
 
-#configure containerd to use the local registry
-minikube ssh <<EOF
+# Configure containerd to use the local registry
+if [[ -n ${CI} ]];
+then
+    sudo mkdir -p /etc/containerd
+    sudo tee /etc/containerd/config.toml <<EOF
+[plugins.cri.registry.mirrors]
+  [plugins.cri.registry.mirrors."localhost"]
+    endpoint = ["http://localhost:5000"]
+EOF
+
+else
+    minikube ssh <<EOF
 # the following commands are executed inside the minikube vm
 # Add the following lines -> see https://github.com/kubernetes/minikube/issues/3444
 sudo sed -i '56i\          endpoint = ["http://localhost:5000"]' /etc/containerd/config.toml  
@@ -36,3 +56,4 @@ sudo sed -i '56i\       [plugins.cri.registry.mirrors."localhost"]' /etc/contain
 sudo systemctl restart containerd
 exit
 EOF
+fi
