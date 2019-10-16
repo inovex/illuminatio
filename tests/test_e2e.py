ddbad45ff823b2ac6d583458b54372a981aaf508
Maximilian Bischoff <maximilian.bischoff@inovex.de>
2019-10-16 20:57:52 +0200

    Fix test implementation

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -14,19 +14,19 @@ E2E_RUNNER_IMAGE = "localhost:5000/illuminatio-runner:dev"
 def kubernetes_utils():
     config.load_kube_config()
     k8s_client = client.ApiClient()
-    return k8s_client, client.CoreV1Api()
+    return k8s_client, client.CoreV1Api(), client.AppsV1Api()
 
 
 @pytest.fixture(autouse=True)
 def clean_cluster(kubernetes_utils):
-    k8s_client, core_v1 = kubernetes_utils
+    yield # below code is executed after test(s)
+    _, core_v1, _ = kubernetes_utils
     # delete e2e namespaces created in test setup
     e2e_namespaces = core_v1.list_namespace(label_selector="illuminatio-e2e")
     for namespace in e2e_namespaces.items:
-        core_v1.delete_namespace(name=namespace.name)
+        core_v1.delete_namespace(name=namespace.metadata.name)
     # delete illuminatio resources
-    res = subprocess.run(["illuminatio", "clean"], capture_output=True)
-    assert res.returncode == 0
+    subprocess.check_output(["illuminatio", "clean"])
 
 
 @pytest.mark.parametrize(
@@ -38,7 +38,7 @@ def clean_cluster(kubernetes_utils):
 @pytest.mark.e2e
 def test__e2e__clean_setup__results_are_expected(e2e_test_case, kubernetes_utils):
     # unpack kubernetes client
-    k8s_client, core_v1 = kubernetes_utils
+    k8s_client, core_v1, apps_v1 = kubernetes_utils
     # get input and expected from test case name
     input_manifest = E2E_INPUT_MANIFEST.format(e2e_test_case)
     expected_yaml = E2E_EXPECTED_YAML.format(e2e_test_case)
@@ -46,18 +46,21 @@ def test__e2e__clean_setup__results_are_expected(e2e_test_case, kubernetes_utils
     utils.create_from_yaml(k8s_client,
                            input_manifest)
     # wait for test resources to be ready
-    wait_for_deployments_ready(namespace=e2e_test_case,
-                               api=k8s_client.AppsV1Api(),
+    wait_for_deployments_ready(e2e_test_case,
+                               api=apps_v1,
                                core_api=core_v1)
     # run illuminatio, with yaml output for later comparison
-    result_file = tempfile.TemporaryFile(suffix=".yaml")
-    cmd = ["illuminatio", "run", f"--runner-image={E2E_RUNNER_IMAGE}", f"-o={result_file}"]
-    res = subprocess.run(cmd, capture_output=True, timeout=120)
-    # assert that command didn't fail
-    assert res.returncode == 0
+    tmp_dir = tempfile.TemporaryDirectory()
+    result_file = open(f"{tmp_dir.name}/result.yaml", "w")
+    cmd = ["illuminatio", "run", "--runner-image", f"{E2E_RUNNER_IMAGE}", "-o", f"{result_file.name}"]
+    res = subprocess.check_output(cmd, timeout=120)
     # load contents of result and expected
-    result = yaml.safe_load(result_file)
-    expected = yaml.safe_load(expected_yaml)
+    stream = open(f"{tmp_dir.name}/result.yaml", "r")
+    result = yaml.safe_load(stream)
+    assert result is not None, f"Could not load result from {stream.name}"
+    expected = yaml.safe_load(open(expected_yaml, "r"))
+    assert expected is not None,  f"Could not load expected from {expected_yaml}"
     # assert that the correct cases have been generated and results match
     assert "cases" in result
+    print(yaml.dump(result["cases"]))
     assert expected == result["cases"]
--- tests/utils.py
+++ tests/utils.py
@@ -12,16 +12,11 @@ def wait_for_deployments_ready(e2_test_case,
     Checks e2_test_case's namespaces for Deployments and waits until all are fully ready
     """
     label_selector = f"illuminatio-e2e={e2_test_case}"
-    print(f"Getting e2e namespaces with label_selector {label_selector}")
-    namespace_list = core_api.list_namespace(label_selector=label_selector)
-    namespaces = [n.metadata.name for n in namespace_list.items]
     tries = 0
-    print(f"Ensure that Pods of Deployments in {namespaces} are ready")
+    print(f"Ensure that Pods of Deployments with labels {label_selector} are ready")
     while tries <= max_tries:
         try:
-            deployments = []
-            for ns in namespaces:
-                deployments += api.list_namespaced_deployment(namespace=ns)
+            deployments = api.list_deployment_for_all_namespaces(label_selector=label_selector)
             if all([_deployment_ready(d) for d in deployments.items]):
                 return
         except k8s.client.rest.ApiException as api_exception:
@@ -29,10 +24,10 @@ def wait_for_deployments_ready(e2_test_case,
         time.sleep(sleep_time)
         tries += 1
     waited_time = tries * sleep_time
-    pytest.fail(f"Deployments in {namespaces} have not come up in {waited_time}s")
+    pytest.fail(f"Deployments in {label_selector} have not come up in {waited_time}s")
 
 
 def _deployment_ready(deployment):
-    ready = deployment.status.number_ready
-    scheduled = deployment.status.desired_number_scheduled
+    ready = deployment.status.ready_replicas or 0
+    scheduled = deployment.status.replicas or 0
     return scheduled > 0 and scheduled == ready
