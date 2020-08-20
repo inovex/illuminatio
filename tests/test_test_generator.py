43dc671a21ecd93d6ada51dd95f4500b0dfe11c7
Maximilian Bischoff <maximilian.bischoff@inovex.de>
2020-08-20 13:54:38 +0200

    Reproduce #102

--- tests/test_test_generator.py
+++ tests/test_test_generator.py
@@ -231,6 +231,81 @@ gen = NetworkTestCaseGenerator(logging.getLogger("test_test_generator"))
             ],
             id="Allow labelled Pods to communicate to Pods with the same labels in the same Namespace",
         ),
+        pytest.param(
+            [k8s.client.V1Namespace(metadata=k8s.client.V1ObjectMeta(name="default"))],
+            [
+                k8s.client.V1NetworkPolicy(
+                    metadata=k8s.client.V1ObjectMeta(
+                        name="allow-labelled-pods", namespace="default"
+                    ),
+                    spec=k8s.client.V1NetworkPolicySpec(
+                        pod_selector=k8s.client.V1LabelSelector(match_labels={}),
+                        ingress=[
+                            k8s.client.V1NetworkPolicyIngressRule(
+                                _from=[
+                                    k8s.client.V1NetworkPolicyPeer(
+                                        pod_selector=k8s.client.V1LabelSelector(
+                                            match_labels={
+                                                "test.io/test-123_XYZ": "test_456-123.ABC"
+                                            }
+                                        )
+                                    )
+                                ],
+                                ports=[
+                                    k8s.client.V1NetworkPolicyPort(
+                                        port=None, protocol="TCP"
+                                    )
+                                ],
+                            )
+                        ],
+                    ),
+                )
+            ],
+            [
+                NetworkTestCase(
+                    ClusterHost(
+                        "default", {"test.io/test-123_XYZ": "test_456-123.ABC"}
+                    ),
+                    ClusterHost("default", {}),
+                    "*",
+                    True,
+                ),
+                NetworkTestCase(
+                    ClusterHost(
+                        "default",
+                        {
+                            INVERTED_ATTRIBUTE_PREFIX
+                            + "test.io/test-123_XYZ": "test_456-123.ABC"
+                        },
+                    ),
+                    ClusterHost("default", {}),
+                    "*",
+                    False,
+                ),
+                NetworkTestCase(
+                    ClusterHost(
+                        INVERTED_ATTRIBUTE_PREFIX + "default",
+                        {"test.io/test-123_XYZ": "test_456-123.ABC"},
+                    ),
+                    ClusterHost("default", {}),
+                    "*",
+                    False,
+                ),
+                NetworkTestCase(
+                    ClusterHost(
+                        INVERTED_ATTRIBUTE_PREFIX + "default",
+                        {
+                            INVERTED_ATTRIBUTE_PREFIX
+                            + "test.io/test-123_XYZ": "test_456-123.ABC"
+                        },
+                    ),
+                    ClusterHost("default", {}),
+                    "*",
+                    False,
+                ),
+            ],
+            id="Correctly handle portless NetworkPolicy",
+        ),
     ],
 )
 def test__generate_test_cases(namespaces, networkpolicies, expected_testcases):
